c3f7a7a80f3d01b6943f0c9f6bfa4d5739b80e9e
barisAdaliDag <barisadali.dag@hotmail.com>
2024-09-21 20:51:54 +0300

    2.video mimari hazırlama

---
+++ lib/product/constants/padding_constans.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+
+// class AppPadding {
+//   static const EdgeInsets small = EdgeInsets.all(8);
+//   static const EdgeInsets medium = EdgeInsets.all(16);
+//   static const EdgeInsets large = EdgeInsets.all(24);
+
+//   static const EdgeInsets horizontalSmall = EdgeInsets.symmetric(horizontal: 8);
+//   static const EdgeInsets horizontalMedium = EdgeInsets.symmetric(horizontal: 16);
+//   static const EdgeInsets horizontalLarge = EdgeInsets.symmetric(horizontal: 24);
+
+//   static const EdgeInsets verticalSmall = EdgeInsets.symmetric(vertical: 8);
+//   static const EdgeInsets verticalMedium = EdgeInsets.symmetric(vertical: 16);
+//   static const EdgeInsets verticalLarge = EdgeInsets.symmetric(vertical: 24);
+// }
